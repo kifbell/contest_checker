b988bbc76a55ff684cb22e6ef7e43ade3cbc8cc8
Kirill Belyakov <dog.kirill.belyakov@gmail.com>
2021-10-29 16:00:38 +0300

    robocop and transfer modes are added

--- contest_checker.py
+++ contest_checker.py
@@ -59,17 +59,21 @@ def read_contest_number() -> int:
         n = int(input("Insert the number of contest file (contest_<num>.csv): "))
     return n
 
+
 def check_student_names_file_exists():
     if not file_exists("student_names.csv"):
-        raise Exception("File with student names (student_names.csv) is not found!")
+        raise Exception(
+            "File with student names (student_names.csv) is not found!")
+
 
 def create_empty_dir(dir_to_copy):
     '''creates empty dir to copy solutions in'''
     try:
-        os.rmdir(dir_to_copy) 
+        os.rmdir(dir_to_copy)
     except:
         pass
     os.makedirs(dir_to_copy, exist_ok=True)
+
 
 def get_student_name(current_dir):
     '''extract student name surname from dir'''
@@ -78,7 +82,8 @@ def get_student_name(current_dir):
     try:
         return match.group(1)
     except:
-        print("problem with " + current_dir) 
+        print("problem with " + current_dir)
+
 
 def check_files(path, tokens: List[str]):
     """main func. It goes through all the solutions to find those with forbiden methods"""
@@ -91,21 +96,53 @@ def check_files(path, tokens: List[str]):
 
             for file in files:
                 if file != ".DS_Store" and "OK.py" in file:
-                    path_to_solution = os.path.join(dir_to_copy, current_dir, file)
-                    found_tokens = fop.check_out_tokens(path_to_solution, tokens)
+                    path_to_solution = os.path.join(
+                        dir_to_copy, current_dir, file)
+                    found_tokens = fop.check_out_tokens(
+                        path_to_solution, tokens)
 
-                    if found_tokens:    
+                    if found_tokens:
                         fop.copy_solution(path_to_solution, student_name)
 
     print(f'all the solutions have been checked')
 
-if __name__ == "__main__":
-    # marks_to_xlsx()
-    path = "/Users/fuckingbell/Downloads/submits"
-    if not path:
-        dir_name = input("enter archive dir: ")
-    tokens = list("if")
-    if not tokens:
-        tokens = list(input("enter tokens: ").split())
+
+def define_mode():
+    """input a working mode"""
+    mode_index = input("Enter the number of the mode you want to work in: "
+                       "\n1 — transfer marks to excel;"
+                       "\n2 — find all solutions with forbiden methods in.\n").strip()
+
+    if mode_index not in ('1', '2', '3'):
+        while True:
+            mode_index = input("Enter the number of the mode you want to work in: "
+                               "\n1 — transfer marks to excel;"
+                               "\n2 — find all solutions with forbiden methods in.\n").strip()
+            if mode_index in ('1', '2'):
+                break
+    return ("transfer", "find")[int(mode_index) - 1]
+
+
+def robocop():
+    default_path = "/Users/fuckingbell/Downloads/submits"
+    archive_path = input("enter dir the solutions archive:"
+                         f"\n'default' will use {default_path} "
+                         "as a path.\n").strip()
+    
+    if archive_path == "default":
+        archive_path = default_path
+    tokens = list(input("enter the list tokens using spaces: ").split())
 
     check_files(path, tokens)
+
+
+def opperate(mode):
+    if mode == "transfer":
+        marks_to_xlsx()
+    else:
+        robocop()
+
+
+if __name__ == "__main__":
+    # marks_to_xlsx()
+    opperate(define_mode())
